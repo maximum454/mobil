e8af9eec6e4f3fc3523df18e1b8b9852bb7ca143
Maxim <maximum454@gmail.com>
2015-09-01 22:33:14 +0600

    step2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $(function(){
+
+	$(".bx-slider").bxSlider({
+		auto:1,
+		pager:0,
+		controls:1
+	});
+
 	$('.popap_box').click(function() {
 		$('#modalbox_' + $(this).data('body')).arcticmodal();
 	});
